a46a175114ea912770a7a306055801e04a87f545
emieli <you@example.com>
2024-01-25 09:09:52 +0000

    Add get_range() function to reduce code duplication
    Also renamed many variables to make code easier to read.
    
    Removed things like "f_" at the start of variables as it didn't add any useful information.
    
    Changed "s_" variables to "spine_", and more.

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import yaml
 import jinja2
 
 
-def main():
+def get_range(my_range: str) -> list:
+    range_split = my_range.split("-")
+    return list(range(int(range_split[0]), int(range_split[1]) + 1))
+
+
+def main() -> None:
     # get configuration variables
     with open("etc/cfg.yml", "r") as cfg:
         config = yaml.load(cfg.read(), Loader=yaml.Loader)
@@ -17,37 +22,31 @@ def main():
     spines = []
     leaves = []
 
-    # generate IP and ASN
+    """Generate lists of subnets"""
     loopbacks = list(IPNetwork(config["ip"]["loopbacks"]).subnet(32))
-    f_ptp_subnet = list(IPNetwork(config["ip"]["fabric_ptp"]).subnet(31))
+    subnets = list(IPNetwork(config["ip"]["fabric_ptp"]).subnet(31))
 
-    f_if_range_spine = config["spines"]["fabric_interfaces"].split("-")
-    f_if_list_spine = list(
-        range(int(f_if_range_spine[0]), int(f_if_range_spine[1]) + 1)
-    )
-
-    f_if_range_leaf = config["leaves"]["fabric_interfaces"].split("-")
-    f_if_list_leaf = list(range(int(f_if_range_leaf[0]), int(f_if_range_leaf[1]) + 1))
-
-    f_asn_range = config["ip"]["asn_range"].split("-")
-    f_asn_list = list(range(int(f_asn_range[0]), int(f_asn_range[1]) + 1))
+    """Generate list of interfaces/as-numbers [1, 2, 3, 4] from input '1-4' """
+    spine_interfaces = get_range(config["spines"]["fabric_interfaces"])
+    leaf_interfaces = get_range(config["leaves"]["fabric_interfaces"])
+    bgp_as_numbers = get_range(config["ip"]["asn_range"])
 
     # add spines
     for i in range(1, int(config["spines"]["number"]) + 1):
-        s_name = "spine" + str(i)
-        s_loopback = str(loopbacks.pop(0))
-        s_as = str(f_asn_list.pop(0))
+        spine_name = "spine" + str(i)
+        spine_loopback = str(loopbacks.pop(0))
+        spine_as = str(bgp_as_numbers.pop(0))
 
         G.add_node(
-            s_name,
-            label=s_name + "\nlo0: " + s_loopback + "\nAS:" + s_as,
-            hostname=s_name,
+            spine_name,
+            label=spine_name + "\nlo0: " + spine_loopback + "\nAS:" + spine_as,
+            hostname=spine_name,
             role="spine",
             shape="box",
-            asn=s_as,
+            asn=spine_as,
             style="rounded,filled",
-            f_int=f_if_list_spine.copy(),
-            f_if_ip={},
+            int=spine_interfaces.copy(),
+            if_ip={},
             constraint="false",
             rank="same; spine1; spine2; ",
             tailport="s",
@@ -55,41 +54,41 @@ def main():
             bgp_neigh={},
             fillcolor="grey88",
         )
-        spines.append(s_name)
+        spines.append(spine_name)
 
     # add leaves
     for i in range(1, int(config["leaves"]["number"]) + 1):
-        l_name = "leaf" + str(i)
-        l_loopback = str(loopbacks.pop(0))
-        l_as = str(f_asn_list.pop(0))
+        leaf_name = "leaf" + str(i)
+        leaf_loopback = str(loopbacks.pop(0))
+        leaf_as = str(bgp_as_numbers.pop(0))
         G.add_node(
-            l_name,
-            label=l_name + "\nlo0: " + l_loopback + "\nAS: " + l_as,
-            hostname=l_name,
+            leaf_name,
+            label=leaf_name + "\nlo0: " + leaf_loopback + "\nAS: " + leaf_as,
+            hostname=leaf_name,
             role="leaf",
             shape="box",
             style="filled",
-            f_int=f_if_list_leaf.copy(),
-            f_if_ip={},
+            int=leaf_interfaces.copy(),
+            if_ip={},
             bgp_neigh={},
-            URL=l_name + ".svg",
+            URL=leaf_name + ".svg",
             headport="s",
-            asn=l_as,
+            asn=leaf_as,
             fontsize=10,
             fillcolor="honeydew2",
         )
-        leaves.append(l_name)
+        leaves.append(leaf_name)
 
     # connect leaves to spines
     for leaf in leaves:
         for sp in spines:
-            f_ptp = f_ptp_subnet.pop(0)
-            spine_if = G.nodes[sp]["f_int"].pop(0)
-            leaf_if = G.nodes[leaf]["f_int"].pop(0)
-            spine_ip = str(IPAddress(f_ptp.first)) + "/31"
-            leaf_ip = str(IPAddress(f_ptp.last)) + "/31"
-            G.nodes[sp]["f_if_ip"].update({"eth" + str(spine_if): spine_ip})
-            G.nodes[leaf]["f_if_ip"].update({"eth" + str(leaf_if): leaf_ip})
+            ptp = subnets.pop(0)
+            spine_if = G.nodes[sp]["int"].pop(0)
+            leaf_if = G.nodes[leaf]["int"].pop(0)
+            spine_ip = str(IPAddress(ptp.first)) + "/31"
+            leaf_ip = str(IPAddress(ptp.last)) + "/31"
+            G.nodes[sp]["if_ip"].update({"eth" + str(spine_if): spine_ip})
+            G.nodes[leaf]["if_ip"].update({"eth" + str(leaf_if): leaf_ip})
             G.nodes[sp]["bgp_neigh"].update(
                 {leaf_ip.split("/")[0]: G.nodes[leaf]["asn"]}
             )
@@ -99,7 +98,7 @@ def main():
             G.add_edge(
                 sp,
                 leaf,
-                l_ptp=f_ptp,
+                leaf_ptp=ptp,
                 decorate="true",
                 e_taillabel="e"
                 + str(spine_if)
@@ -120,14 +119,14 @@ def main():
 
     # draw leaf to spine relation
     for le in leaves:
-        l_neighbors = list(G.predecessors(le))
-        l_sub = l_neighbors + [le]
-        L = G.subgraph(l_sub)
+        leaf_neighbors = list(G.predecessors(le))
+        leaf_sub = leaf_neighbors + [le]
+        L = G.subgraph(leaf_sub)
         L.nodes[le]["URL"] = "../config/" + le + ".txt"
-        for l_edge in L.edges:
-            L.edges[l_edge]["label"] = L.edges[l_edge]["l_ptp"]
-            L.edges[l_edge]["taillabel"] = L.edges[l_edge]["e_taillabel"]
-            L.edges[l_edge]["headlabel"] = L.edges[l_edge]["e_headlabel"]
+        for leaf_edge in L.edges:
+            L.edges[leaf_edge]["label"] = L.edges[leaf_edge]["leaf_ptp"]
+            L.edges[leaf_edge]["taillabel"] = L.edges[leaf_edge]["e_taillabel"]
+            L.edges[leaf_edge]["headlabel"] = L.edges[leaf_edge]["e_headlabel"]
 
         LG = nx.drawing.nx_agraph.to_agraph(L)
         LG.layout("dot")
